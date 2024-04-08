fdb0898abcf122df2dd7a218d791fecc66239a51
raafarosa <141688193+raafarosa@users.noreply.github.com>
2024-04-08 15:34:50 -0300

    Commit

--- Module 2 - Tabelas em SQL/Query/Query_2.sql
+++ Module 2 - Tabelas em SQL/Query/Query_2.sql
@@ -1,1 +1,1 @@
-SELECT id, 	valor_transacoes_12m FROM clientes WHERE escolaridade = 'mestrado' 	and sexo = 'F';
+SELECT id, valor_transacoes_12m FROM clientes WHERE escolaridade = 'mestrado' 	and sexo = 'F';
